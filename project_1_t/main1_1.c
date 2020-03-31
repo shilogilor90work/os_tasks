758c19da9517e46d1e47f6086bf994789327a407
shilo <shilo@xfind.ai>
2020-03-31 16:14:13 +0300

    added task_1

---
+++ project_1_t/main1_1.c
@@ -0,0 +1,14 @@
+
+#include "hello_ariel.h"
+
+
+void main1_1()
+{
+    hello_ariel();
+}
+
+int main()
+{
+    hello_ariel();
+    return 0;
+}
---
+++ project_1_t/main1_2.c
@@ -0,0 +1,13 @@
+
+extern void hello_ariel;
+
+void main1_2()
+{
+    hello_ariel();
+}
+
+int main()
+{
+    hello_ariel();
+    return 0;
+}
