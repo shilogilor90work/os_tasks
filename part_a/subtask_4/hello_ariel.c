2eddb754d90efadea7ba26154fd56dbf68714e24
shilo <shilo@xfind.ai>
2020-04-12 18:11:49 +0300

    ready almost to hand in

---
+++ part_a/subtask_4/hello_ariel.c
@@ -0,0 +1,6 @@
+#include "hello_ariel.h"
+
+void hello_ariel()
+{
+    printf("Hello Ariel");
+}
---
+++ part_a/subtask_4/main1_3.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdbool.h>
+
+#include <stdio.h>
+#include <dlfcn.h>
+
+
+void (*hello_ariel)();
+
+bool read_lib()
+{
+    void *myso = dlopen("./libhello.so", RTLD_LAZY);
+    if (myso == NULL) {
+        return false;
+    }
+    hello_ariel = (void(*)())dlsym(myso, "hello_ariel");
+    if (hello_ariel == NULL) {
+        return false;
+    }
+    return true;
+}
+int main() {
+    if (read_lib()) {
+        hello_ariel();
+    }
+    else {
+        printf("lib not created yet!");
+    }
+    return 0;
+}
