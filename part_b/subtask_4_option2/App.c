2e1289c289a2a18291e1739ffe531573ec0d6bc3
shilo <shilo@xfind.ai>
2020-04-13 11:46:45 +0300

    int

--- part_b/subtask_4_option2/App.c
+++ part_b/subtask_4_option2/App.c
@@ -57,8 +57,8 @@ int task_3(void *parmas)
         syslog(LOG_NOTICE, "doing....");
         usleep(5000000);
         syslog(LOG_NOTICE, "finished");
-    } else {    
-        for (i=0;i<25;i++) {
+    } else {
+        for (int i=0;i<25;i++) {
             print("Daemon PID %d\n", pid3);
             usleep(1000000);
         }
